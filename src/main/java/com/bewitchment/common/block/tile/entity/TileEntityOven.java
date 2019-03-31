59b4b467a9236051b4a5bef26bf9748e100eb5c6
MoriyaShiine <moriyashiine@gmail.com>
2019-03-31 11:08:02 -0500

    oven

--- src/main/java/com/bewitchment/common/block/tile/entity/TileEntityOven.java
+++ src/main/java/com/bewitchment/common/block/tile/entity/TileEntityOven.java
@@ -10,6 +10,7 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
@@ -18,12 +19,20 @@ public class TileEntityOven extends ModTileEntity implements ITickable
 {
 	public int burn_time, fuel_burn_time, progress;
 	
+	private OvenRecipe recipe;
+	
 	public final ItemStackHandler inventory_up = new ItemStackHandler(3)
 	{
 		@Override
 		public boolean isItemValid(int index, ItemStack stack)
 		{
 			return index == 0 ? TileEntityFurnace.isItemFuel(stack) : index == 1 ? stack.getItem() == ModObjects.empty_jar : true;
+		}
+		
+		@Override
+		protected void onContentsChanged(int index)
+		{
+			recipe = BewitchmentAPI.REGISTRY_OVEN.getValuesCollection().parallelStream().filter(p -> p.matches(getStackInSlot(2))).findFirst().orElse(null);
 		}
 	};
 	public final ItemStackHandler inventory_down = new ItemStackHandler(2)
@@ -39,7 +48,6 @@ public class TileEntityOven extends ModTileEntity implements ITickable
 	public void update()
 	{
 		if (burn_time > 0) burn_time--;
-		OvenRecipe recipe = BewitchmentAPI.REGISTRY_OVEN.getValuesCollection().parallelStream().filter(p -> p.matches(inventory_up.getStackInSlot(2))).findFirst().orElse(null);
 		if (recipe == null) progress = 0;
 		else if (recipe.canOutputFit(inventory_down))
 		{
@@ -76,6 +84,7 @@ public class TileEntityOven extends ModTileEntity implements ITickable
 	@Override
 	public NBTTagCompound writeToNBT(NBTTagCompound tag)
 	{
+		tag.setString("recipe", recipe == null ? "" : recipe.getRegistryName().toString());
 		tag.setInteger("burn_time", burn_time);
 		tag.setInteger("fuel_burn_time", fuel_burn_time);
 		tag.setInteger("progress", progress);
@@ -86,6 +95,7 @@ public class TileEntityOven extends ModTileEntity implements ITickable
 	public void readFromNBT(NBTTagCompound tag)
 	{
 		super.readFromNBT(tag);
+		recipe = tag.getString("recipe").isEmpty() ? null : BewitchmentAPI.REGISTRY_OVEN.getValue(new ResourceLocation(tag.getString("recipe")));
 		burn_time = tag.getInteger("burn_time");
 		fuel_burn_time = tag.getInteger("fuel_burn_time");
 		progress = tag.getInteger("progress");
