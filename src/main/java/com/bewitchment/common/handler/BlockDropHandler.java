52b1a57bdb81a63ea1aae036ee2eabcc056c7feb
Sunconure11 <eyespot7b@aol.com>
2019-04-18 17:04:01 -0400

    Moved some stuff over to blockdrophandler

--- src/main/java/com/bewitchment/common/handler/BlockDropHandler.java
+++ src/main/java/com/bewitchment/common/handler/BlockDropHandler.java
@@ -1,9 +1,10 @@
 package com.bewitchment.common.handler;
 
 import com.bewitchment.registry.ModObjects;
-import net.minecraft.block.Block;
+import net.minecraft.block.*;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -18,6 +19,20 @@ public class BlockDropHandler {
 		if (event.getState().getBlock() == block && (ignoreSilkTouch ? true : !event.isSilkTouching())) {
 			if (replace) event.getDrops().clear();
 			if (event.getWorld().rand.nextInt(100) <= chance) event.getDrops().add(out);
+		}
+	}
+
+	@SubscribeEvent
+	public static void dropController(BlockEvent.HarvestDropsEvent event) {
+		if (event.getState().getBlock() == Blocks.RED_FLOWER && event.getState().getValue(Blocks.RED_FLOWER.getTypeProperty()) == BlockFlower.EnumFlowerType.ALLIUM && event.getWorld().rand.nextInt(6) == 0) {
+			//Allium -> garlic seeds
+			event.getDrops().clear();
+			event.getDrops().add(new ItemStack(ModObjects.seed_garlic, 1));
+		} else if (((event.getState().getBlock() == Blocks.LEAVES && event.getState().getValue(BlockOldLeaf.VARIANT) == BlockPlanks.EnumType.OAK) || (event.getState().getBlock() == Blocks.LEAVES2 && event.getState().getValue(BlockNewLeaf.VARIANT) == BlockPlanks.EnumType.DARK_OAK)) && event.getWorld().rand.nextInt(24) == 0 && event.getState().getValue(BlockLeaves.DECAYABLE)) {
+			//Oak and dark oak -> apple gall
+			event.getDrops().clear();
+			event.getDrops().add(new ItemStack(ModObjects.oak_apple_gall, 1));
+
 		}
 	}
 
@@ -39,8 +54,5 @@ public class BlockDropHandler {
 
 		replaceDrop(event, Blocks.DEADBUSH, new ItemStack(ModObjects.seed_white_sage), 15, false, false);
 		replaceDrop(event, Blocks.DEADBUSH, new ItemStack(ModObjects.seed_sagebrush), 15, false, false);
-
-		//Set this to drop from oak and dark oak leaves
-		replaceDrop(event, Blocks.LEAVES, new ItemStack(ModObjects.oak_apple_gall), 15, false, false);
 	}
 }
