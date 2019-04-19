359730271530f73584292efdb15f357fe3a727f4
MoriyaShiine <moriyashiine@gmail.com>
2019-04-19 18:00:00 -0500

    black dog eyes

--- src/main/java/com/bewitchment/client/render/entity/spirits/ghosts/RenderBlackDog.java
+++ src/main/java/com/bewitchment/client/render/entity/spirits/ghosts/RenderBlackDog.java
@@ -3,9 +3,12 @@ package com.bewitchment.client.render.entity.spirits.ghosts;
 import com.bewitchment.Bewitchment;
 import com.bewitchment.client.model.entity.spirits.ghosts.ModelBlackDog;
 import com.bewitchment.common.entity.spirits.ghosts.EntityBlackDog;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -21,6 +24,7 @@ public class RenderBlackDog extends RenderLiving<EntityBlackDog> {
 
 	public RenderBlackDog(RenderManager manager) {
 		super(manager, new ModelBlackDog(), 0.3f);
+		addLayer(new LayerEyes(this));
 	}
 
 	@Override
@@ -32,5 +36,44 @@ public class RenderBlackDog extends RenderLiving<EntityBlackDog> {
 	protected void preRenderCallback(EntityBlackDog entity, float partialTickTime) {
 		super.preRenderCallback(entity, partialTickTime);
 		GlStateManager.scale(1.8, 1.8, 1.8);
+	}
+	
+	private static class LayerEyes implements LayerRenderer<EntityBlackDog>
+	{
+		private static final ResourceLocation[] TEX = {
+				new ResourceLocation(Bewitchment.MOD_ID, "textures/entity/black_dog_eyes_0.png"),
+				new ResourceLocation(Bewitchment.MOD_ID, "textures/entity/black_dog_eyes_1.png"),
+				new ResourceLocation(Bewitchment.MOD_ID, "textures/entity/black_dog_eyes_2.png"),
+				new ResourceLocation(Bewitchment.MOD_ID, "textures/entity/black_dog_eyes_3.png"),
+				new ResourceLocation(Bewitchment.MOD_ID, "textures/entity/black_dog_eyes_4.png")};
+		
+		private final RenderBlackDog renderer;
+		
+		public LayerEyes(RenderBlackDog renderer)
+		{
+			this.renderer = renderer;
+		}
+		
+		@Override
+		public void doRenderLayer(EntityBlackDog entity, float limbSwing, float limbSwingAmount, float partialTicks, float age, float rotationYaw, float rotationPitch, float scale)
+		{
+			renderer.bindTexture(TEX[entity.getDataManager().get(EntityBlackDog.SKIN)]);
+			GlStateManager.enableBlend();
+			GlStateManager.disableAlpha();
+			if (entity.isInvisible()) GlStateManager.depthMask(false);
+			else GlStateManager.depthMask(true);
+	        Minecraft.getMinecraft().entityRenderer.setupFogColor(true);
+	        renderer.getMainModel().render(entity, limbSwing, limbSwingAmount, age, rotationYaw, rotationPitch, scale);
+	        Minecraft.getMinecraft().entityRenderer.setupFogColor(false);
+	        renderer.setLightmap(entity);
+			GlStateManager.disableBlend();
+			GlStateManager.enableAlpha();
+		}
+		
+		@Override
+		public boolean shouldCombineTextures()
+		{
+			return false;
+		}
 	}
 }
