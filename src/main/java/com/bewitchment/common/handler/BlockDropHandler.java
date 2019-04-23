2741798304f6418c439060ebd3e0d1bdc9d6b5c7
MoriyaShiine <moriyashiine@gmail.com>
2019-04-23 16:54:36 -0500

    block drop changes + boline gives string when used on sheep

--- src/main/java/com/bewitchment/common/handler/BlockDropHandler.java
+++ src/main/java/com/bewitchment/common/handler/BlockDropHandler.java
@@ -1,51 +1,50 @@
 package com.bewitchment.common.handler;
 
+import java.util.Random;
+import java.util.function.Predicate;
+
 import com.bewitchment.registry.ModObjects;
-import net.minecraft.block.*;
+
+import net.minecraft.block.BlockFlower.EnumFlowerType;
+import net.minecraft.block.BlockNewLeaf;
+import net.minecraft.block.BlockOldLeaf;
+import net.minecraft.block.BlockPlanks.EnumType;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
-import java.util.Random;
 
 public class BlockDropHandler {
 	private static int getFortuneDropAmount(Random rand, int fortuneLevel) {
 		return fortuneLevel > 0 ? Math.max(0, rand.nextInt(fortuneLevel + 2)) : 1;
 	}
 
-	private static void replaceDrop(HarvestDropsEvent event, Block block, ItemStack out, int chance, boolean replace, boolean ignoreSilkTouch) {
-		if (event.getState().getBlock() == block && (ignoreSilkTouch ? true : !event.isSilkTouching())) {
-			if (replace) event.getDrops().clear();
+	private static void replaceDrop(HarvestDropsEvent event, Predicate<IBlockState> predicate, ItemStack out, int chance, boolean replaceAlways, boolean replaceIfTest, boolean ignoreSilkTouch) {
+		boolean test = predicate.test(event.getState());
+		if (test && (ignoreSilkTouch ? true : !event.isSilkTouching())) {
+			if (replaceAlways || (replaceIfTest && test)) event.getDrops().clear();
 			if (event.getWorld().rand.nextInt(100) <= chance) event.getDrops().add(out);
 		}
 	}
 
 	@SubscribeEvent
-	public static void dropController(BlockEvent.HarvestDropsEvent event) {
-		if (event.getState().getBlock() == Blocks.RED_FLOWER && event.getState().getValue(Blocks.RED_FLOWER.getTypeProperty()) == BlockFlower.EnumFlowerType.ALLIUM && event.getWorld().rand.nextInt(6) == 0) {
-			//Allium -> garlic seeds
-			event.getDrops().clear();
-			event.getDrops().add(new ItemStack(ModObjects.seed_garlic, 1));
-		} else if (((event.getState().getBlock() == Blocks.LEAVES && event.getState().getValue(BlockOldLeaf.VARIANT) == BlockPlanks.EnumType.OAK) || (event.getState().getBlock() == Blocks.LEAVES2 && event.getState().getValue(BlockNewLeaf.VARIANT) == BlockPlanks.EnumType.DARK_OAK)) && event.getWorld().rand.nextInt(24) == 0 && event.getState().getValue(BlockLeaves.DECAYABLE)) {
-			//Oak and dark oak -> apple gall
-			event.getDrops().clear();
-			event.getDrops().add(new ItemStack(ModObjects.oak_apple_gall, 1));
-
-		}
-	}
-
-	@SubscribeEvent
 	public void harvestDrops(HarvestDropsEvent event) {
-		replaceDrop(event, ModObjects.salt_ore, new ItemStack(ModObjects.salt, getFortuneDropAmount(event.getWorld().rand, event.getFortuneLevel())), 100, true, false);
-		replaceDrop(event, ModObjects.amethyst_ore, new ItemStack(ModObjects.amethyst, getFortuneDropAmount(event.getWorld().rand, event.getFortuneLevel())), 100, true, false);
-		replaceDrop(event, ModObjects.garnet_ore, new ItemStack(ModObjects.garnet, getFortuneDropAmount(event.getWorld().rand, event.getFortuneLevel())), 100, true, false);
-		replaceDrop(event, ModObjects.moonstone_ore, new ItemStack(ModObjects.moonstone, getFortuneDropAmount(event.getWorld().rand, event.getFortuneLevel())), 100, true, false);
+		replaceDrop(event, s -> s.getBlock() == ModObjects.salt_ore, new ItemStack(ModObjects.salt, getFortuneDropAmount(event.getWorld().rand, event.getFortuneLevel())), 100, true, false, false);
+		replaceDrop(event, s -> s.getBlock() == ModObjects.amethyst_ore, new ItemStack(ModObjects.amethyst, getFortuneDropAmount(event.getWorld().rand, event.getFortuneLevel())), 100, true, false, false);
+		replaceDrop(event, s -> s.getBlock() == ModObjects.garnet_ore, new ItemStack(ModObjects.garnet, getFortuneDropAmount(event.getWorld().rand, event.getFortuneLevel())), 100, true, false, false);
+		replaceDrop(event, s -> s.getBlock() == ModObjects.moonstone_ore, new ItemStack(ModObjects.moonstone, getFortuneDropAmount(event.getWorld().rand, event.getFortuneLevel())), 100, true, false, false);
 
-		replaceDrop(event, ModObjects.leaves_juniper, new ItemStack(ModObjects.juniper_berries), 15, false, false);
-		replaceDrop(event, ModObjects.leaves_yew, new ItemStack(ModObjects.yew_aril), 15, false, false);
+		replaceDrop(event, s -> s.getBlock() == ModObjects.leaves_cypress, new ItemStack(ModObjects.sapling_cypress), 5, true, false, false);
+		replaceDrop(event, s -> s.getBlock() == ModObjects.leaves_elder, new ItemStack(ModObjects.sapling_elder), 5, true, false, false);
+		replaceDrop(event, s -> s.getBlock() == ModObjects.leaves_juniper, new ItemStack(ModObjects.sapling_juniper), 5, true, false, false);
+		replaceDrop(event, s -> s.getBlock() == ModObjects.leaves_yew, new ItemStack(ModObjects.sapling_yew), 5, true, false, false);
+		
+		replaceDrop(event, s -> s.getBlock() == ModObjects.leaves_juniper, new ItemStack(ModObjects.juniper_berries), 1, false, false, false);
+		replaceDrop(event, s -> s.getBlock() == ModObjects.leaves_yew, new ItemStack(ModObjects.yew_aril), 1, false, false, false);
 
-		replaceDrop(event, Blocks.DEADBUSH, new ItemStack(ModObjects.seed_white_sage), 15, false, false);
+		replaceDrop(event, s -> s.getBlock() == Blocks.DEADBUSH, new ItemStack(ModObjects.seed_white_sage), 15, false, true, false);
+		replaceDrop(event, s -> s.getBlock() == Blocks.RED_FLOWER && s.getValue(Blocks.RED_FLOWER.getTypeProperty()) == EnumFlowerType.ALLIUM, new ItemStack(ModObjects.seed_garlic), 15, false, true, false);
+		replaceDrop(event, s -> (s.getBlock() == Blocks.LEAVES && s.getValue(BlockOldLeaf.VARIANT) == EnumType.OAK) || (s.getBlock() == Blocks.LEAVES2 && s.getValue(BlockNewLeaf.VARIANT) == EnumType.DARK_OAK), new ItemStack(ModObjects.oak_apple_gall), 5, false, true, false);
 	}
 }
--- src/main/java/com/bewitchment/common/item/tool/ItemBoline.java
+++ src/main/java/com/bewitchment/common/item/tool/ItemBoline.java
@@ -1,22 +1,35 @@
 package com.bewitchment.common.item.tool;
 
+import java.util.List;
+
 import com.bewitchment.Bewitchment;
+import com.bewitchment.registry.ModObjects;
+import com.google.common.collect.Multimap;
+
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.passive.EntitySheep;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Enchantments;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemShears;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.IShearable;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-
-import java.util.List;
 
 public class ItemBoline extends ItemShears {
 	public ItemBoline() {
@@ -32,16 +45,50 @@ public class ItemBoline extends ItemShears {
 		stack.getTagCompound().setInteger("biome_id", Biome.getIdForBiome(world.getBiome(player.getPosition())));
 		return new ActionResult<>(EnumActionResult.SUCCESS, stack);
 	}
-
+	
+	@Override
+	public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot equipmentSlot)
+	{
+		@SuppressWarnings("deprecation")
+		Multimap<String, AttributeModifier> multimap = super.getItemAttributeModifiers(equipmentSlot);
+		if (equipmentSlot == EntityEquipmentSlot.MAINHAND)
+		{
+			multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", 3 + ModObjects.TOOL_SILVER.getAttackDamage(), 0));
+			multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -2.4, 0));
+		}
+		return multimap;
+	}
+	
 	@Override
 	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
-		if (!target.world.isRemote) {
-			if (attacker instanceof EntityPlayer) {
-				target.attackEntityFrom(DamageSource.causePlayerDamage((EntityPlayer) attacker), 5);
-				stack.damageItem(1, attacker);
-			} else return super.hitEntity(stack, target, attacker);
+		if (!target.world.isRemote) stack.damageItem(1, attacker);
+		return super.hitEntity(stack, target, attacker);
+	}
+	
+	@Override
+	public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer player, EntityLivingBase entity, EnumHand hand)
+	{
+		if (!entity.world.isRemote)
+		{
+			if (entity instanceof IShearable)
+			{
+				IShearable target = (IShearable) entity;
+				if (target.isShearable(stack, entity.world, entity.getPosition()))
+				{
+					List<ItemStack> drops = target.onSheared(stack, entity.world, entity.getPosition(), EnchantmentHelper.getEnchantmentLevel(Enchantments.FORTUNE, stack));
+					for(ItemStack stack0 : drops)
+					{
+						EntityItem item = entity.entityDropItem(entity instanceof EntitySheep && stack0.getItem() == Item.getItemFromBlock(Blocks.WOOL) ? new ItemStack(Items.STRING, 4) : stack0, 1);
+						item.motionY += entity.getRNG().nextFloat() * 0.05F;
+						item.motionX += (entity.getRNG().nextFloat() - entity.getRNG().nextFloat()) * 0.1f;
+						item.motionZ += (entity.getRNG().nextFloat() - entity.getRNG().nextFloat()) * 0.1f;
+					}
+					stack.damageItem(1, entity);
+				}
+				return true;
+			}
 		}
-		return true;
+		return false;
 	}
 
 	@Override
