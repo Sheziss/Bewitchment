c852a28793f7b5e877df4e7b53bb2f00dff357aa
MoriyaShiine <moriyashiine@gmail.com>
2019-04-24 21:10:50 -0500

    get outtie

--- src/main/java/com/bewitchment/registry/ModEnchantments.java
+++ src/main/java/com/bewitchment/registry/ModEnchantments.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.bewitchment.Bewitchment;
-import com.bewitchment.common.enchantment.EnchantmentDesperateWard;
-import com.bewitchment.common.enchantment.EnchantmentNaturalAttuning;
-import com.bewitchment.common.enchantment.EnchantmentPotentWard;
 import com.bewitchment.common.enchantment.EnchantmentSpiritProtection;
 
 import net.minecraft.enchantment.Enchantment;
@@ -17,11 +14,6 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @EventBusSubscriber(modid = Bewitchment.MODID)
 public class ModEnchantments {
 	public static final List<Enchantment> REGISTRY = new ArrayList<>();
-
-	public static final Enchantment desperate_ward = new EnchantmentDesperateWard();
-	public static final Enchantment potent_ward = new EnchantmentPotentWard();
-
-	public static final Enchantment natural_attuning = new EnchantmentNaturalAttuning();
 
 	public static final EnchantmentSpiritProtection spirit_protection = new EnchantmentSpiritProtection();
 
