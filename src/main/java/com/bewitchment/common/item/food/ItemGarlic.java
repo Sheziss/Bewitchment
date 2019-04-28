1a6f450271c546513bd032c3c5e28d1dc622c51e
MoriyaShiine <moriyashiine@gmail.com>
2019-04-27 20:00:22 -0500

    simplify crops

--- src/main/java/com/bewitchment/common/item/food/ItemGarlic.java
+++ src/main/java/com/bewitchment/common/item/food/ItemGarlic.java
@@ -1,10 +1,11 @@
 package com.bewitchment.common.item.food;
 
+import com.bewitchment.api.capability.extendedplayer.ExtendedPlayer;
+import com.bewitchment.api.capability.extendedplayer.ExtendedPlayer.TransformationType;
 import com.bewitchment.common.item.util.ModItemFood;
-import com.bewitchment.registry.ModPotions;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 
 public class ItemGarlic extends ModItemFood {
@@ -16,6 +17,6 @@ public class ItemGarlic extends ModItemFood {
 	@Override
 	protected void onFoodEaten(ItemStack stack, World world, EntityPlayer player) {
 		super.onFoodEaten(stack, world, player);
-		player.addPotionEffect(new PotionEffect(ModPotions.garlicked, 20));
+		if (player.getCapability(ExtendedPlayer.CAPABILITY, null).getTransformation() == TransformationType.VAMPIRE) player.setFire(10);
 	}
 }
